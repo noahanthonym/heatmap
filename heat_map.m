2f63a1299e84b9012bd7793c262d42bd34d88ad2
noahanthonym <78045059+noahanthonym@users.noreply.github.com>
2021-03-04 22:31:49 -0500

    Update heat_map.m

--- heat_map.m
+++ heat_map.m
@@ -14,19 +14,22 @@ cd(save_folder)
 uiwait(msgbox('Select data for heat map','modal')); % Selects heat map data
 file_nm = uigetfile();
 load(file_nm)
+table = struct2cell(imported_data);
 
 uiwait(msgbox('Select file with column names for heat map','modal')); %Selects column names
 column_names = uigetfile();
 load(column_names)
-table = cell2mat(struct2cell(imported_data));
-xnames = struct2cell(imported_data2);
+names = struct2cell(imported_data);
 
+axis = names{1,1};
+xvalues = axis(1,2:width(axis));
+yvalues = axis(2:height(axis),1);
+table = table{1,1};
 
 dlg_ans  = inputdlg({'What is your graph title?','What is your x-axis representing?', 'What is your y-axis representing?'});
-yvalues = {'1','2','3','4','5','6','7','8','9','10','11','12','13','14'};
-xvalues = xnames{1,1};
+
 map = heatmap(xvalues, yvalues, table);
 map.Title = dlg_ans(1,1);
 map.XLabel = dlg_ans(2,1);
 map.YLabel = dlg_ans(3,1);
-map.Colormap = summer; % https://www.mathworks.com/help/matlab/ref/colormap.html
+map.Colormap = winter; % https://www.mathworks.com/help/matlab/ref/colormap.html
