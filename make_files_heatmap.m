fc02e3d538d4251d8cbdbe2559fa15157279b3ba
noahanthonym <78045059+noahanthonym@users.noreply.github.com>
2021-03-09 13:59:31 -0500

    Update make_files_heatmap.m

--- make_files_heatmap.m
+++ make_files_heatmap.m
@@ -1,5 +1,5 @@
-% use this script to make files for the use of heatmap.m. 
-% Before running this script, create an empty folder where the data will be saved. 
+% use this script to make files for analysis. Before running this script,
+% create an empty folder where the data will be saved. 
 
 clear
 close all
@@ -7,13 +7,14 @@ clc
 
 save_folder = uigetdir('' , 'Select folder to save data');
 
+uiwait(msgbox('Reminder: One heatmap needs two files, data and column/row names'));
 import_arrays = str2double(cell2mat(inputdlg('Enter the number of arrays to import')));
 
 [import_file , import_path] = uigetfile('*.*' , 'Select file to import data');
 cd(import_path) 
 for stepper = 1:import_arrays;
-    file_nm = inputdlg('Enter the file name you want to use for the data');
-    file_nm = char(file_nm);
+    file_nm = inputdlg('Enter the file name you want to use for the data'); %% takes input of name for array and assigned it dlg_nm
+    file_nm = char(file_nm); %% turns dlg_nm into a text scalar
     imported_data = uiimport(import_file); 
     cd(save_folder)
     save(file_nm, 'imported_data');
