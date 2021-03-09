79068e03092c546679bc0faa8f350ace141427c3
noahanthonym <78045059+noahanthonym@users.noreply.github.com>
2021-03-09 14:49:40 -0500

    Update make_files_heatmap.m

--- make_files_heatmap.m
+++ make_files_heatmap.m
@@ -5,9 +5,10 @@ clear
 close all
 clc
 
+uiwait(msgbox('Select folder to save data'));
 save_folder = uigetdir('' , 'Select folder to save data');
 
-uiwait(msgbox('Reminder: One heatmap needs two files, data and column/row names'));
+uiwait(msgbox('Reminder: One heatmap needs a file with your data numbers and text'));
 import_arrays = str2double(cell2mat(inputdlg('Enter the number of arrays to import')));
 
 [import_file , import_path] = uigetfile('*.*' , 'Select file to import data');
